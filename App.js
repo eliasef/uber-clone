fadfa91f0028bcfd90c759edd750fac3a6483f9e
eliasef <eliasef0305@gmail.com>
2023-06-05 13:16:41 -0300

    :rocket: first commit!

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store'
+import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <StatusBar />
+        <HomeScreen />
+    </Provider>
   );
 }
 
